16e2313ab458d4471844f8a1383ae8faac10e0fb
RhythmOfRiora <cburn01@qub.ac.uk>
2021-06-10 20:09:05 +0100

    Adding Google Forms integration code.

---
+++ google_forms_integration.js
@@ -0,0 +1,59 @@
+var POST_URL = "<PUT WEBHOOK URL HERE>";
+
+
+// This code was taken from a tutorial :)
+function onSubmit(e) {
+    var gform = FormApp.getActiveForm();
+    var allResponses = form.getResponses();
+    var latestResponse = allResponses[allResponses.length - 1];
+    var response = latestResponse.getItemResponses();
+    var items = [];
+
+    for (var i = 0; i < response.length; i++) {
+        var question = response[i].getItem().getTitle();
+        var answer = response[i].getResponse();
+        try {
+            var parts = answer.match(/[\s\S]{1,1024}/g) || [];
+        } catch (e) {
+            var parts = answer;
+        }
+
+        if (answer == "") {
+            continue;
+        }
+        for (var j = 0; j < parts.length; j++) {
+            if (j == 0) {
+                items.push({
+                    "name": question,
+                    "value": parts[j],
+                    "inline": false
+                });
+            } else {
+                items.push({
+                    "name": question.concat(" (cont.)"),
+                    "value": parts[j],
+                    "inline": false
+                });
+            }
+        }
+    }
+
+    var options = {
+        "method": "post",
+        "headers": {
+            "Content-Type": "application/json",
+        },
+        "payload": JSON.stringify({
+            "content": "‌",
+            "embeds": [{
+                "title": "Code Of Conduct Submitted",
+              "color": 33023, 
+                "fields": items,
+                "footer": {
+                    "text": "Some footer here"
+                }
+            }]
+        })
+    };
+
+    UrlFetchApp.fetch(POST_URL, options);

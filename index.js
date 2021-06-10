4be5944dc849b028fbfc45efd9da2d2c363c7a13
RhythmOfRiora <cburn01@qub.ac.uk>
2021-06-10 20:04:57 +0100

    Adding some comments and taking out unnecessary bits.

--- index.js
+++ index.js
@@ -12,32 +12,29 @@ client.on('ready', () => {
   });
 
 client.on('message', msg => {
-    if (msg.content.includes('hey')) {
 
-      let role = msg.guild.roles.cache.find(role => role.name === "Server Member")
+    if (msg.author.bot){
 
-      console.log(msg.guild.members.cache.toJSON())
-
-
-    } else if (msg.author.bot){
+      // Get the Discord name from the Google form.
        var member_id = ""
-
        let responses = msg.embeds[0].fields
 
        responses.forEach(function (arrayItem) {
-       var value = arrayItem.value;
-       console.log(`Form Submission ${value}`);
-
-       if (arrayItem.name.includes('Discord Username')){
-           member_id = String(arrayItem.value);
-           console.log(` Discord ID: ${member_id}`)
-           
-           if (member_id.length > 2 && member_id.length < 32) {
-
-           let role = msg.guild.roles.cache.find(role => role.name === "beginner")
-           let member = msg.guild.members.cache.find(mem => mem.displayName === member_id)
+          var value = arrayItem.value;
+          console.log(`Form Submission ${value}`);
           
-           member.roles.add(role)
+          // If we get to the question asking for their Discord username...
+          if (arrayItem.name.includes('Discord Username')){
+              member_id = String(arrayItem.value);
+              console.log(` Discord ID: ${member_id}`)
+              
+              // Do at least a little validation.
+              if (member_id.length > 2 && member_id.length < 32) {
+                
+                // Assign the role. 
+                let role = msg.guild.roles.cache.find(role => role.name === "beginner")
+                let member = msg.guild.members.cache.find(mem => mem.displayName === member_id)
+                member.roles.add(role)
            }
        }
 
